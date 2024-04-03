f406987ec133d41c03b00363f2dd57d7bfd0f117
The Effrontery <soroush.salari83@gmail.com>
2024-04-03 09:21:02 +0330

    Added the OCR service

---
+++ src/Infrastructure/Data/Migrations/20240403044257_FixOneToManyRelationship.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ImageBrowser.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixOneToManyRelationship : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Files_OwnerId",
+                table: "Files");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Files_OwnerId",
+                table: "Files",
+                column: "OwnerId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Files_OwnerId",
+                table: "Files");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Files_OwnerId",
+                table: "Files",
+                column: "OwnerId",
+                unique: true);
+        }
+    }
+}
---
+++ src/Infrastructure/Services/OCRService.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ImageBrowser.Application.Common.Interfaces;
+using IronOcr;
+using MassTransit.Caching.Internals;
+using Microsoft.AspNetCore.Http;
+
+namespace ImageBrowser.Infrastructure.Services;
+public class OCRService : IOCRService
+{
+    public async Task<List<string>> ExtractText(FileStream file)
+    {
+        string TempPath = Path.GetTempPath();
+        string fileName = Path.GetFileName(file.Name);
+        string fullName = Path.Combine(TempPath, fileName);
+        try
+        {
+
+            using (FileStream fsSource = new FileStream(fullName, FileMode.Create))
+            {
+                file.CopyTo(fsSource);
+                fsSource.Close();
+            }
+
+          
+
+
+                var ocrTesseract = new IronTesseract()
+                {
+                    Language = OcrLanguage.EnglishBest,
+                    Configuration = new TesseractConfiguration()
+                    {
+                        ReadBarCodes = false,
+                        RenderHocr = true,
+                        BlackListCharacters = "`ë|^",
+                        PageSegmentationMode = TesseractPageSegmentationMode.AutoOsd,
+                    }
+                };
+
+                using var ocrInput = new OcrInput();
+                ocrInput.LoadImage(fullName);
+                var ocrResult = ocrTesseract.Read(ocrInput);
+                Console.WriteLine(ocrResult.Text);
+            return ocrResult.Words.Select(w => w.Text).ToList();
+        }
+        catch(Exception ex) {
+            throw new Exception(ex.Message);
+        }
+        finally
+        {
+            
+        }
+
+
+        throw new NotImplementedException();
+    }
+}
+
