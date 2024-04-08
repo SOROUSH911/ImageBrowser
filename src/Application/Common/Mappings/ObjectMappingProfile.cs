5de362676c513ebfad4e7f3543f52080b3196b73
The Effrontery <soroush.salari83@gmail.com>
2024-04-08 22:39:15 +0330

    Added DynamicObjectMapping and serviceInjection into autoMapper

---
+++ src/Application/Common/Mappings/ObjectMappingProfile.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+using ImageBrowser.Application.Common.Interfaces;
+using ImageBrowser.Application.Common.Models;
+
+namespace ImageBrowser.Application.Common.Mappings;
+public class ObjectMappingProfile : Profile
+{
+
+    const string bucketName = "simpleimagebrowser";
+    private readonly IFileProvider fileProvider;
+    private readonly IAppUserIdService appUser;
+
+    public ObjectMappingProfile()
+    {
+
+    }
+    public ObjectMappingProfile(IAppUserIdService appUser, IFileProvider fileProvider)
+    {
+        this.fileProvider = fileProvider;
+        this.appUser = appUser;
+        CreateMap<Domain.Entities.File, FileDto>()
+            .ForMember(s => s.ObjectUrl, d => d.MapFrom(o => fileProvider.GeneratePreSignedURL(bucketName, o.Path, 2).Result));
+    }
+}
--- src/Application/DependencyInjection.cs
+++ src/Application/DependencyInjection.cs
@@ -3,6 +3,8 @@ using CleanArchitectureTemplate.Domain.Mapping;
 using ImageBrowser.Application.Common.Behaviours;
 using ImageBrowser.Application.Common.Middlewares;
 using Microsoft.AspNetCore.Http;
+using ImageBrowser.Application.Common.Mappings;
+using ImageBrowser.Application.Common.Interfaces;
 
 namespace Microsoft.Extensions.DependencyInjection;
 
@@ -11,6 +13,12 @@ public static class DependencyInjection
     public static IServiceCollection AddApplicationServices(this IServiceCollection services)
     {
         services.AddAutoMapper(config => config.AddProfile(new MappingProfile()));
+
+        services.AddSingleton(provider => new MapperConfiguration(config =>
+        {
+            var scope = provider.CreateScope();
+            config.AddProfile(new ObjectMappingProfile(scope.ServiceProvider.GetService<IAppUserIdService>(), scope.ServiceProvider.GetService<IFileProvider>()));
+        }));
 
         services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
 
